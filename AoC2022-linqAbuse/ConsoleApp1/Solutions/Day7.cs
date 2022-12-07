a5ecb4352c155dcd419dee1a28ced3dfcc4fe2c9
SirWaffle <18519779+SirWaffle@users.noreply.github.com>
2022-12-07 13:23:53 -0600

    day7done

--- AoC2022-linqAbuse/ConsoleApp1/Solutions/Day7.cs
+++ AoC2022-linqAbuse/ConsoleApp1/Solutions/Day7.cs
@@ -11,85 +11,80 @@ namespace ConsoleApp1.Solutions
 {
     internal class Day7 : AbstractPuzzle
     {
-        override public void Part1()
+        public void Both()
         {
-            var split = File.ReadAllText(InputFile!)
-                .Split("\n")
-                .Select((s, i) => s.Contains('$')? (0, i, s.Trim()): (1, i, s.Trim()))
-                .GroupBy(x => x.Item1);
-
             Dictionary<string, (Int64, List<(Int64, string)>)> fs = new Dictionary<string, (Int64, List<(Int64, string)>)>() { { "", (0, new()) } };
             List<string> curPath = new List<string>() { };
 
-            var cmds = split.ToList().SelectMany(g => g.ToList().Where(x => x.Item1 == 0));
+            //split into lists
+            var splitLists = File.ReadAllText(InputFile!)
+                .Split("\n")
+                .Select((s, i) => s.Contains('$') ? (0, i, s.Trim()) : (1, i, s.Trim()))
+                .GroupBy(x => x.Item1)
+                .Select(x => (x.ToList()
+                                .Where(x => x.Item1 == 0).ToList(),
+                              x.ToList()
+                                .Where(x => x.Item1 == 1).ToList()))
+                .Select(x => x.Item1.Count != 0? x.Item1: x.Item2)
+                .ToList();
 
-            var files = split.ToList().SelectMany(g => g.ToList().Where(x => x.Item1 == 1));
-
-            foreach (var cmd in cmds)
+            //create dir structure
+            foreach (var cmd in splitLists[0])
             {
                 if(cmd.Item3.Contains("cd"))
                 {
-                    var cmdSplit = cmd.Item3.Split(" ");
-                    if (cmdSplit[2] == "..")
-                    {
+                    if (cmd.Item3.Split(" ")[2] == "..")
                         curPath.RemoveAt(curPath.Count - 1);
-                    }
                     else
-                    {
-                        curPath.Add(cmdSplit[2] == "/"? "/root": cmdSplit[2]);
-                    }
-
-                    string filepath = curPath.Aggregate((x, y) => x + (x == "/" ? "" : "/") + y);
-                    (Int64, List<(Int64, string)>) entry;
-                    if (!fs.TryGetValue(filepath, out entry))
-                    {
-                        entry = new(0, new());
-                        fs.Add(filepath, entry);
-                    }
+                        curPath.Add(cmd.Item3.Split(" ")[2] == "/"? "/root": cmd.Item3.Split(" ")[2]); 
                 }
 
-                if(cmd.Item3.Contains("ls"))
+                string filepath = curPath.Aggregate((x, y) => x + (x == "/" ? "" : "/") + y);
+                (Int64, List<(Int64, string)>) entry;
+                if (!fs.TryGetValue(filepath, out entry))
+                    fs.Add(filepath, new(0, new()));
+
+                if (cmd.Item3.Contains("ls"))
                 {
                     int ind = cmd.i;
-                    var filesInDir = files.SkipWhile(x => x.i < cmd.i).TakeWhile(x => x.i == ++ind ).ToList();
-                    string filepath = curPath.Aggregate( (x, y) => x + (x == "/"? "" : "/") + y);
-                    foreach(var file in filesInDir)
+                    var filesInDir = splitLists[1].SkipWhile(x => x.i < cmd.i).TakeWhile(x => x.i == ++ind ).ToList();
+                    foreach (var file in filesInDir)
                     {
-                        var fSplit = file.Item3.Split(" ");
-                        if (fSplit[0] == "dir")
-                            continue;
-
-                        (Int64, List<(Int64, string)>) entry = new(fs[filepath].Item1, fs[filepath].Item2);
-
-                        int size = int.Parse(fSplit[0]);
-
-                        entry.Item1 += size;
-                        entry.Item2.Add( (int.Parse(fSplit[0]), fSplit[1]) );
-
-                        fs[filepath] = entry;
+                        if (file.Item3.Split(" ")[0] != "dir")
+                        {
+                            entry.Item1 += int.Parse(file.Item3.Split(" ")[0]);
+                            entry.Item2.Add((int.Parse(file.Item3.Split(" ")[0]), file.Item3.Split(" ")[1]));
+                            fs[filepath] = entry;
+                        }
                     }
                 }
             }
 
-            //somehow calculate values for all dirs without recursion and *hopefully* without loops
+            //calculate directory sizes
             fs.OrderByDescending(x => x.Key.Where(x => x == '/').Count())
               .Where(x => x.Key != "").ToList()
               .ForEach(x => fs[x.Key.Substring(0, x.Key.LastIndexOf('/'))] = new(fs[x.Key.Substring(0, x.Key.LastIndexOf('/'))].Item1 + fs[x.Key].Item1,fs[x.Key.Substring(0, x.Key.LastIndexOf('/'))].Item2));
 
-            var sum = fs
+            Console.WriteLine("sum: " + fs
                 .Where(x => x.Value.Item1 <= 100000)
-                .Sum(x => x.Value.Item1);
+                .Sum(x => x.Value.Item1)
+            );
 
-            //2104783 is proper
-            Console.WriteLine("sum: " + sum.ToString());
+            Console.WriteLine( "To Free: " + fs
+                .Where(x => x.Value.Item1 >= 30000000 - (70000000 - fs["/root"].Item1))
+                .Min(x => x.Value.Item1)
+            );
+        }             
+        //2104783 is correct sum
+
+        override public void Part1()
+        {
+            Both();
         }
 
         override public void Part2()
         {
-            Console.WriteLine("First message: " + (File.ReadAllText(InputFile!)
-                .Split("\n\r").Select(s => s
-                    .TakeWhile((c, i) => s.Substring(i, 14).ToCharArray().Distinct().Count() != 14)
-                ).ToList()[0].Count() + 14));
+            Both();
         }
     }
 }
