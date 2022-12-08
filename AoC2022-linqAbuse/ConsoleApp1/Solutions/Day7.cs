8e9ddc5eaeae52769c2d62c135f3614f9fe27ce5
SirWaffle <18519779+SirWaffle@users.noreply.github.com>
2022-12-07 20:33:47 -0600

    7 ;'s for day 7

--- AoC2022-linqAbuse/ConsoleApp1/Solutions/Day7.cs
+++ AoC2022-linqAbuse/ConsoleApp1/Solutions/Day7.cs
@@ -13,28 +13,17 @@ namespace ConsoleApp1.Solutions
     internal class Day7 : AbstractPuzzle
     {
 
-        //11 ;'s
+        //7 ;'s
         public void Both()
         {
-            Dictionary<string, Int64> fs = new Dictionary<string, Int64>() { { "", 0 } };
-            List<string> curPath = new List<string>() { };
+            var fsandpath = ( new Dictionary<string, Int64>() { { "", 0 } }, new List<string>() { }, new List<List<(int,int,string)>>() );
 
-            //split into lists
-            var splitLists = File.ReadAllText(InputFile!).Split("\n").Select((s, i) => s.Contains('$') ? (0, i, s.Trim()) : (1, i, s.Trim())).GroupBy(x => x.Item1).Select(x => (x.ToList().Where(x => x.Item1 == 0).ToList(), x.ToList().Where(x => x.Item1 == 1).ToList())).Select(x => x.Item1.Count != 0? x.Item1: x.Item2).ToList();
-
-            splitLists[0].Select(x => (x.Item2, x.Item3.Contains("cd") ? x.Item3.Split(" ")[2] : "ls")).ToList()
-                .Select(cmd => cmd.Item2 == "ls"
-                ? new Task(() => { int ind = cmd.Item1; fs.Add(curPath.Aggregate((x, y) => x + (x == "/" ? "" : "/") + y), splitLists[1].SkipWhile(x => x.i < cmd.Item1).TakeWhile(x => x.i == ++ind).ToList().Where(x => x.Item3.Split(" ")[0] != "dir").Select(x => int.Parse(x.Item3.Split(" ")[0])).Sum()); })
-                : cmd.Item2 == ".."
-                    ? new Task(() => curPath.RemoveAt(curPath.Count - 1))
-                    : new Task(() => curPath.Add(cmd.Item2 == "/" ? "/root" : cmd.Item2))
-                ).All(x => { x.Start(); x.Wait(); return true; });
-
-            //sum directory sizes
-            fs.OrderByDescending(x => x.Key.Where(x => x == '/').Count()).Where(x => x.Key != "").ToList().ForEach(x => fs[x.Key.Substring(0, x.Key.LastIndexOf('/'))] = fs[x.Key.Substring(0, x.Key.LastIndexOf('/'))] + fs[x.Key]);
-
-            //answers
-            Console.WriteLine("sum: " + fs.Where(x => x.Value <= 100000).Sum(x => x.Value) + "  need to free: " + fs.Where(x => x.Value >= 30000000 - (70000000 - fs["/root"])).Min(x => x.Value) );
+            new Action[4] {
+                () => fsandpath.Item3 = File.ReadAllText(InputFile!).Split("\n").Select((s, i) => s.Contains('$') ? (0, i, s.Trim()) : (1, i, s.Trim())).GroupBy(x => x.Item1).Select(x => (x.ToList().Where(x => x.Item1 == 0).ToList(), x.ToList().Where(x => x.Item1 == 1).ToList())).Select(x => x.Item1.Count != 0? x.Item1: x.Item2).ToList(),
+                () => fsandpath.Item3 [0].Select(x => (x.Item2, x.Item3.Contains("cd") ? x.Item3.Split(" ")[2] : "ls")).ToList().Select(cmd => cmd.Item2 == "ls"? new Task(() => { int ind = cmd.Item1; fsandpath.Item1.Add(fsandpath.Item2.Aggregate((x, y) => x + (x == "/" ? "" : "/") + y), fsandpath.Item3 [1].SkipWhile(x => x.Item2 < cmd.Item1).TakeWhile(x => x.Item2 == ++ind).ToList().Where(x => x.Item3.Split(" ")[0] != "dir").Select(x => int.Parse(x.Item3.Split(" ")[0])).Sum()); }): cmd.Item2 == ".."? new Task(() => fsandpath.Item2.RemoveAt(fsandpath.Item2.Count - 1)): new Task(() => fsandpath.Item2.Add(cmd.Item2 == "/" ? "/root" : cmd.Item2))).All(x => { x.Start(); x.Wait(); return true; }),
+                () => fsandpath.Item1.OrderByDescending(x => x.Key.Where(x => x == '/').Count()).Where(x => x.Key != "").ToList().ForEach(x => fsandpath.Item1[x.Key.Substring(0, x.Key.LastIndexOf('/'))] = fsandpath.Item1[x.Key.Substring(0, x.Key.LastIndexOf('/'))] + fsandpath.Item1[x.Key]),
+                () => Console.WriteLine("sum: " + fsandpath.Item1.Where(x => x.Value <= 100000).Sum(x => x.Value) + "  need to free: " + fsandpath.Item1.Where(x => x.Value >= 30000000 - (70000000 - fsandpath.Item1["/root"])).Min(x => x.Value) )
+            }.ToList().ForEach(x => x());
         }             
         //2104783 is correct sum
 
