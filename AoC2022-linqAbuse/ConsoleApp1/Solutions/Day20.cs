70df33d2f468fbbe3f78375386ae09dfc2687c8f
SirWaffle <18519779+SirWaffle@users.noreply.github.com>
2022-12-20 13:27:20 -0600

    slight changes

--- AoC2022-linqAbuse/ConsoleApp1/Solutions/Day20.cs
+++ AoC2022-linqAbuse/ConsoleApp1/Solutions/Day20.cs
@@ -25,14 +25,14 @@ namespace ConsoleApp1.Solutions
 
         override public void Part1()
         {
-            Both();
+            Both(false);
         }
 
 
 
         override public void Part2()
         {
-            Both();
+            Both(true);
         }
 
         public class NodeInfo
@@ -57,36 +57,41 @@ namespace ConsoleApp1.Solutions
             }
         }
 
-        void Both()
+        void Both(bool part2)
         {
             var lines = File.ReadAllText(InputFile!).Split("\r\n", StringSplitOptions.RemoveEmptyEntries).Select(x => x.Trim());
             var nums = lines.Select((x, i) => new NodeInfo(i, Int64.Parse(x.Trim()), Int64.Parse(x.Trim() ))).ToList();
 
-            //for part two, we make the numbers huge. pretty sure we can modulo here to reduce how much we have to scan around and shit
+            //for part two, we make the numbers huge. pretty sure we can modulo here to reduce how much we have to scan
             var mixed = new LinkedList<NodeInfo>(nums);
 
-            //TODO: need to make sure we move based on the original order of the nodes...annoying
-            //use the originalIndex to find them in order and move them
-            //needed for passes past 1.
+            int mixAmount = 1;
+            if (part2)
+                mixAmount = 10;
 
-            for (int mixCount = 0; mixCount < 10; ++mixCount)
+            for (int mixCount = 0; mixCount < mixAmount; ++mixCount)
             {
                 LinkedListNode<NodeInfo> curNode = mixed.First!;
 
                 for (var node = mixed.First; ; node = node.Next)
                 {
-                    node.ValueRef.originalP2Value = node.ValueRef.originalValue * 811589153;
-                    node.ValueRef.value = node.ValueRef.originalValue * 811589153;
-                    if (node.ValueRef.value / 811589153 != node.ValueRef.originalValue)
+                    if (part2)
                     {
-                        throw new Exception("overflow");
+                        node.ValueRef.originalP2Value = node.ValueRef.originalValue * 811589153;
+                        node.ValueRef.value = node.ValueRef.originalValue * 811589153;
                     }
-                    node.ValueRef.value = (node.ValueRef.value % (nums.Count - 1));// * Math.Sign(node.ValueRef.value);
+                    else
+                    {
+                        node.ValueRef.value = node.ValueRef.originalValue;
+                    }
+
+                    node.ValueRef.value = (node.ValueRef.value % (nums.Count - 1));
                     //if(Math.Sign(node.ValueRef.originalValue) < 0)
                     //{
                     //    node.ValueRef.value = (nums.Count - 1) + node.ValueRef.value;
                     //}
                     node.ValueRef.moveAmount = node.ValueRef.value;
+                    
                     if (node.Next == null)
                         break;
                 }
@@ -126,25 +131,19 @@ namespace ConsoleApp1.Solutions
                         toMoveNode.ValueRef.moveAmount -= moveDir;
 
                         if (moveDir > 0)
-                        {
                             moveTo = moveTo!.Next == null ? mixed.First! : moveTo!.Next!;
-                        }
                         else
-                        {
                             moveTo = moveTo!.Previous == null ? mixed.Last! : moveTo!.Previous!;
-                        }
 
                         if (moveTo == toMoveNode)
-                        {
                             throw new Exception("shouldnt loop over ourselves");
-                        }
                     }
 
                     //Console.Write(" to " + moveTo.Value.ToString());
-                    //it seems like we dont ever move to the front, instead should be added to back isntead of inserted at first...
-                    if(toMoveNode != moveTo)
-                        mixed.Remove(toMoveNode);
+                    
+                    mixed.Remove(toMoveNode);
 
+                    //it seems like we dont ever move to the front, instead should be added to back isntead of inserted at first...
                     if (moveDir < 0 && moveTo == mixed.First)
                         mixed.AddAfter(mixed.Last, toMoveNode.Value);
                     else if (moveDir > 0)
@@ -193,7 +192,11 @@ namespace ConsoleApp1.Solutions
 
                     if (score == 1000 || score == 2000 || score == 3000)
                     {
-                        scoreSum += node.ValueRef.originalP2Value;
+                        if(part2)
+                            scoreSum += node.ValueRef.originalP2Value;
+                        else
+                            scoreSum += node.ValueRef.originalValue;
+
                         Console.WriteLine("Score node: " + node.Value);
                         scored++;
 
